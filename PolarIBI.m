7a9da22e8f1716c79c715744050e9c49ea6acb8b
Mark M. Span <m.m.span@gmail.com>
2021-04-09 09:44:57 +0200

    Update PolarIBI.m

--- PolarIBI.m
+++ PolarIBI.m
@@ -9,39 +9,43 @@ f_y = polyval(p,(1:numel(ecg))',[],mu);
 
 ECG_data = ecg - f_y;        % Detrend data
 plot(tmOrig,ECG_data, 'k-')
+
+%% RESAMPLE USING CUBIC SPLINE METHOD
+% 'new' time axis
 tm = tmOrig(1):.001:tmOrig(end);
 % cubic spline interpolationj
 ECG_data = spline(tmOrig,ECG_data,tm);
 %ECG_data = ecg;              % or keep it RAW
-%% R-Top Trigger
+%% Plot the ECG trace data
 hold on
 plot(tm,ECG_data)
 grid on
 title('Detrended ECG Signal')
 xlabel('Samples')
 ylabel('Voltage(mV)')
-
+%% R-Top Trigger
 [~,locs_Rwave] = findpeaks(ECG_data,'MinPeakHeight',400,...
                                     'MinPeakDistance',50);
 
-%%  PLOT
+%%  PLOT the Triggered R-tops
 hold on
 t = tm(locs_Rwave);
-
 plot(t,ECG_data(locs_Rwave),'ro')
 
 ml = max(ylim);
 nd = (ECG_data(locs_Rwave)*0) + ml;
 
 plot(t,nd,'rv','MarkerFaceColor','r')
+%% Plot the IBI values
 ibi = round(diff(tm(locs_Rwave)),3);
 for i = 1:length(ibi)-1
     text((t(i)+t(i+1))/2,double(nd(i))-50,int2str(ibi(i)*1000), 'FontSize', 8, 'HorizontalAlignment', 'center')
 end
 hold off
 legend('Detrended ECG Signal', 'R-Top')
-
+%% Plot ibi values histogram
 figure(2)
 histogram(ibi,40)
+%% plot the IBI values over time
 figure(3)
 plot(ibi)
