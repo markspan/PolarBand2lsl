ba1e63a292331bacbcac98f7851361c827647e4e
Mark M. Span <m.m.span@gmail.com>
2021-04-13 23:53:25 +0200

    revert to matlab

--- README.md
+++ README.md
@@ -31,6 +31,8 @@ You can record the stream with [Labrecorder](https://github.com/labstreaminglaye
 A sample script for peak detection is also provided. Based on [Matlab Documentation](https://nl.mathworks.com/help/wavelet/ug/r-wave-detection-in-the-ecg.html]).
 This script uses the xdf import module of LabStreamingLayer (https://github.com/xdf-modules/xdf-Matlab), and the 'findpeaks' function from the MATLAB Signal Processing Toolbox
 
+Also a ibi detection function base on the ibi interpolation created by A.R.van Roon for CARSPAN is provided
+
 ![Screenshot 2021-02-25 115853](https://user-images.githubusercontent.com/4105112/110318793-40345100-800e-11eb-9f86-872d7848a1ac.png)
 # Stolen from:
 [Pareeknikhil](https://towardsdatascience.com/creating-a-data-stream-with-polar-device-a5c93c9ccc59)
---
+++ getIBI.m
@@ -0,0 +1,87 @@
+function [ibis, t, par] = getIBI(ecgData, ecgTimestamps, varargin)
+%  GETIBI Detects the IBI values from a ECG trace. Using interpolation to get ms. precision
+% 
+% Algorithm developed by A.M. van Roon for PRECAR (CARSPAN preprocessing).
+% 
+% Matlab version M.M.Span (2021)
+%%  default values:
+par.fSample = 130;
+par.MinPeakHeight = 310;
+par.MinPeakDistance = 50;
+par.deTrend = true;
+par.Plot = true;
+%% Parse the name - value pairs found in varargin
+%------------------------------------------------------------------------------------------
+for arg = 1:2:length(varargin)
+    % is the name a parameter?
+    if isfield(par,varargin{arg})
+        % and does it have a value?
+        if length(varargin)<=arg
+            ME = MException('getIBI:MissingValue', ...
+                'Parameter %s should be followed by a parameter',varargin{arg});
+            throw(ME)
+        end
+        par.(varargin{arg}) = varargin{arg+1};
+    else
+        ME = MException('getIBI:noSuchParameter', ...
+            'Parameter %s does not exist',varargin{arg});
+        throw(ME)
+    end
+end
+%% ------------------------------------------------------------------------------------------
+% are we asked to detrend the data? Detrend algorithm in this file (below).
+if (par.deTrend)
+   ecgData = deTrend(ecgData);
+end
+%% Then, first find the (approximate) peaks
+[vals,locs] = findpeaks(ecgData,'MinPeakHeight',par.MinPeakHeight,...
+    'MinPeakDistance',par.MinPeakDistance);
+disp(['*found '  int2str(length(vals))  ' r-tops'])
+%% Now the algorithm can start.
+%------------------------------------------------------------------------------------------
+rc = max(abs(vals - ecgData(locs - 1)), abs(ecgData(locs + 1) - vals));
+try
+    correction =  (ecgData(locs + 1) - ecgData(locs - 1)) / par.fSample / 2 ./ abs(rc);
+catch ME
+    causeException = MException('MATLAB:getIBI:divisionbyzero', 'rc is zero at some point in the data');
+    ME = addCause(ME,causeException);
+    rethrow(ME);
+end
+t = ecgTimestamps(locs) + correction';
+ibis = round(diff(t),3);
+if (par.Plot) % Plot algorithm in this file (below)
+    PlotECGWithIBIS(ecgTimestamps,ecgData,locs,t)
+end
+end
+%%
+function ecgData = deTrend(ecgData)
+    [p,~,mu] = polyfit((1:numel(ecgData))',ecgData,6);
+    f_y = polyval(p,(1:numel(ecgData))',[],mu);
+    ecgData = ecgData - f_y;        % Detrend data
+end
+function PlotECGWithIBIS(ecgTimestamps,ecgData,locs,rtops)
+    %% Plot the ECG trace data
+    ibis = round(diff(rtops),3);
+    hold on
+    plot(ecgTimestamps,ecgData)
+    grid on
+    title('(Detrended) ECG Signal (mV)')
+    xlabel('time (sec)')
+    ylabel('Voltage(mV)')
+    
+    %%  PLOT the Triggered R-tops
+    hold on
+    plot(rtops,ecgData(locs),'ro')
+    
+    %% also plot thim at the top of ther plot:
+    ml = max(ylim);
+    nd = (ecgData(locs)*0) + ml;
+    plot(rtops,nd,'rv','MarkerFaceColor','r')
+    
+    %% Plot the IBI value labels in between the top r-top markers:
+    for i = 1:length(ibis)-1
+        text(double(rtops(i)+rtops(i+1))/2,double(nd(i))-50,int2str(ibis(i)*1000), 'FontSize', 8, 'HorizontalAlignment', 'center')
+    end
+    hold off
+    legend('Detrended ECG Signal', 'R-Top')    
+end
